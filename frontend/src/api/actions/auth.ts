9a2d5976c2149e23ca6ea20ffba69e6d8cee7298
pathaaay <aayush.pathak@roimaint.com>
2026-02-05 19:13:08 +0530

    change login action api

--- frontend/src/api/actions/auth.ts
+++ frontend/src/api/actions/auth.ts
@@ -2,8 +2,8 @@ import { apiService } from "@/lib/axios";
 import type { LoginSchemaType } from "@/lib/schemas/login-schema";
 
 export const login = async (data: LoginSchemaType) => {
-  const res = await apiService.get("/auth/login", {
-    data,
+  const res = await apiService.post("/auth/login", {
+    ...data,
   });
-  return res;
+  return res.data;
 };
