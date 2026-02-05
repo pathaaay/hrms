c4c3dd8cbbb788a77b0ad333d6505fa8c994827e
pathaaay <aayush.pathak@roimaint.com>
2026-02-05 17:54:10 +0530

    add login form

--- frontend/src/pages/auth/login-page.tsx
+++ frontend/src/pages/auth/login-page.tsx
@@ -1,5 +1,7 @@
+import { LoginForm } from "@/components/forms/auth/login-form";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <LoginForm />;
 };
 
 export default LoginPage;
