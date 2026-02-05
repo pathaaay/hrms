62cbe17ecccf5e1930d0206169c9b2671eda028d
pathaaay <aayush.pathak@roimaint.com>
2026-02-05 17:10:03 +0530

    add toaster and configure routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import RootLayout from "./pages/RootLayout.tsx";
+import AuthLayout from "./pages/auth/auth-layout.tsx";
+import LoginPage from "./pages/auth/login-page.tsx";
+import NotFoundPage from "./components/common/not-found.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+        ],
+      },
+
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
