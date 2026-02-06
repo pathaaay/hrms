4e217ddfbc7d4542ff3635aa137f44036137114f
pathaaay <aayush.pathak@roimaint.com>
2026-02-06 18:20:59 +0530

    add navbar

--- frontend/src/pages/protected/protected-layout.tsx
+++ frontend/src/pages/protected/protected-layout.tsx
@@ -1,11 +1,14 @@
 import { fetchUser } from "@/api/queries/user";
+import { Navbar } from "@/components/common/navbar";
 import { Outlet } from "react-router";
 
 const ProtectedLayout = () => {
   const { data, isFetching } = fetchUser();
 
+
   return (
     <>
+      <Navbar userProfile={data} />
       <Outlet context={{ data, isFetching }} />
     </>
   );
