a1a93112bd38f223155389fa8ed350bdb216023d
pathaaay <aayush.pathak@roimaint.com>
2026-02-06 13:02:16 +0530

    add user profile initial state

--- frontend/src/store/slices/user-slice.ts
+++ frontend/src/store/slices/user-slice.ts
@@ -1,13 +1,22 @@
+import type { IUserProfileType } from "@/lib/types/user";
 import { createSlice } from "@reduxjs/toolkit";
+
+interface IInitialState {
+  isAuthenticated: boolean;
+  userProfile: IUserProfileType | null;
+}
+
+const initialState = {
+  isAuthenticated: false,
+  userProfile: null,
+} as IInitialState;
 
 export const userSlice = createSlice({
   name: "user-slice",
-  initialState: {
-    user: null,
-  },
+  initialState,
   reducers: {
     setUser(state, { payload }) {
-      state.user = payload;
+      state.userProfile = payload;
     },
   },
 });
