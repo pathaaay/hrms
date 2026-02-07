e0069b0b78de1af7b9d10a21e57edcbf68943fd2
pathaaay <aayush.pathak@roimaint.com>
2026-02-07 14:48:40 +0530

    add some icons to the game card

--- frontend/src/components/games/game-card.tsx
+++ frontend/src/components/games/game-card.tsx
@@ -8,6 +8,14 @@ import {
   CardTitle,
 } from "../ui/card";
 import { Button } from "../ui/button";
+import {
+  ClockIcon,
+  Gamepad2Icon,
+  RepeatIcon,
+  TimerIcon,
+  UsersIcon,
+  type LucideIcon,
+} from "lucide-react";
 
 interface GameCardProps {
   game: IGame;
@@ -15,22 +23,32 @@ interface GameCardProps {
 
 export const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card className="rounded-2xl shadow-md hover:shadow-xl transition-all duration-300">
+    <Card className="rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 border-muted">
       <CardHeader>
         <CardTitle className="flex justify-between items-center">
-          <span className="text-xl font-semibold">{game.name}</span>
-          <Badge variant={"secondary"}>
+          <div className="flex items-center gap-2">
+            <Gamepad2Icon />
+            <span className="text-xl font-semibold">{game.name}</span>
+          </div>
+          <Badge variant={"secondary"} className="flex items-center gap-1">
+            <ClockIcon className="size-3!" />
             {game.startTime}:00 - {game.endTime}:00
           </Badge>
         </CardTitle>
       </CardHeader>
       <CardContent className="space-y-3 text-sm text-muted-foreground">
-        <CardContentRow label={"Max Players"} value={game.maxPlayersPerSlot} />
         <CardContentRow
+          Icon={UsersIcon}
+          label={"Max Players"}
+          value={game.maxPlayersPerSlot}
+        />
+        <CardContentRow
+          Icon={TimerIcon}
           label={"Slot Duration"}
           value={game.maxSlotDurationInMinutes + "mins"}
         />
         <CardContentRow
+          Icon={RepeatIcon}
           label={"Booking Cycle"}
           value={game.bookingCycleHours + "hrs"}
         />
@@ -43,15 +61,20 @@ export const GameCard = ({ game }: GameCardProps) => {
 };
 
 const CardContentRow = ({
+  Icon,
   label,
   value,
 }: {
+  Icon: LucideIcon;
   label: string;
   value: string | number;
 }) => {
   return (
     <div className="flex justify-between">
-      <span>{label}</span>
+      <div className="flex items-center gap-2">
+        <Icon className="size-4!" />
+        <span>{label}</span>
+      </div>
       <span className="font-medium text-foreground">{value}</span>
     </div>
   );
