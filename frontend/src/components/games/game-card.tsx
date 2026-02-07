cfb214ad6bcee80a32e8c3190e0d8fffe69f60a0
pathaaay <aayush.pathak@roimaint.com>
2026-02-07 14:32:48 +0530

    optimize game card

--- frontend/src/components/games/game-card.tsx
+++ frontend/src/components/games/game-card.tsx
@@ -1,11 +1,18 @@
 import type { IGame } from "@/lib/types/game";
 import { Badge } from "../ui/badge";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 import { Button } from "../ui/button";
 
 interface GameCardProps {
   game: IGame;
 }
+
 export const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card className="rounded-2xl shadow-md hover:shadow-xl transition-all duration-300">
@@ -18,28 +25,34 @@ export const GameCard = ({ game }: GameCardProps) => {
         </CardTitle>
       </CardHeader>
       <CardContent className="space-y-3 text-sm text-muted-foreground">
-        <div className="flex justify-between">
-          <span>Max Players</span>
-          <span className="font-medium text-foreground">
-            {game.maxPlayersPerSlot}
-          </span>
-        </div>
-        <div className="flex justify-between">
-          <span>Slot Duration</span>
-          <span className="font-medium text-foreground">
-            {game.maxSlotDurationInMinutes} mins
-          </span>
-        </div>
-        <div className="flex justify-between">
-          <span>Booking Cycle</span>
-          <span className="font-medium text-foreground">
-            {game.bookingCycleHours} hrs
-          </span>
-        </div>
+        <CardContentRow label={"Max Players"} value={game.maxPlayersPerSlot} />
+        <CardContentRow
+          label={"Slot Duration"}
+          value={game.maxSlotDurationInMinutes + "mins"}
+        />
+        <CardContentRow
+          label={"Booking Cycle"}
+          value={game.bookingCycleHours + "hrs"}
+        />
       </CardContent>
       <CardFooter>
         <Button className="w-full mt-4 rounded-xl">Book Now</Button>
       </CardFooter>
     </Card>
+  );
+};
+
+const CardContentRow = ({
+  label,
+  value,
+}: {
+  label: string;
+  value: string | number;
+}) => {
+  return (
+    <div className="flex justify-between">
+      <span>{label}</span>
+      <span className="font-medium text-foreground">{value}</span>
+    </div>
   );
 };
