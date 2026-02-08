a11cca9fcf1163fb255473106c98f6585a76e2e1
pathaaay <aayush.pathak@roimaint.com>
2026-02-08 11:18:19 +0530

    add edit profile form and also the custom form component

---
+++ frontend/src/components/forms/user/edit-profile-form.tsx
@@ -0,0 +1,68 @@
+import { useEditProfileMutation } from "@/api/mutations/user";
+import { CustomFormFields } from "@/components/shared/custom-form-fields";
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldSet } from "@/components/ui/field";
+
+import {
+  EditProfileSchema,
+  type EditProfileSchemaType,
+} from "@/lib/schemas/profile-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+export const EditProfile = () => {
+  const { mutate: handleEditProfile, isPending } = useEditProfileMutation();
+
+  const form = useForm({
+    resolver: zodResolver(EditProfileSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      timezone: "",
+    },
+  });
+
+  const onFormSubmit = (values: EditProfileSchemaType) => {
+    handleEditProfile(values);
+  };
+
+  const editProfileFormFields: {
+    label: string;
+    key: keyof EditProfileSchemaType;
+    placeholder: string;
+  }[] = [
+    {
+      label: "Email",
+      key: "email",
+      placeholder: "email",
+    },
+    {
+      label: "name",
+      key: "name",
+      placeholder: "name",
+    },
+    {
+      label: "Timezone",
+      key: "timezone",
+      placeholder: "Select Timezone",
+    },
+  ];
+
+  return (
+    <form onSubmit={form.handleSubmit(onFormSubmit)}>
+      <FieldSet className="w-full">
+        <FieldGroup>
+          <CustomFormFields<EditProfileSchemaType>
+            control={form.control}
+            formFields={editProfileFormFields}
+          />
+          <Field>
+            <Button type="submit" disabled={isPending}>
+              {isPending ? "Update Profile" : "Please wait..."}
+            </Button>
+          </Field>
+        </FieldGroup>
+      </FieldSet>
+    </form>
+  );
+};
