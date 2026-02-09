6ce73f931d724c7ce3f8b2dc6050d607602fb96d
pathaaay <aayush.pathak@roimaint.com>
2026-02-09 16:32:46 +0530

    add datatable for game booking page

--- frontend/src/pages/protected/games/bookings/index.tsx
+++ frontend/src/pages/protected/games/bookings/index.tsx
@@ -1,9 +1,13 @@
+import DataTableWithColumnFilterDemo from "@/components/common/data-table";
 import { GoBackBtn } from "@/components/shared/go-back-btn";
 
 export const UserGameBookingPage = () => {
   return (
     <div className="flex flex-col items-center gap-2 relative">
       <GoBackBtn to={"/games"} />
+      <div className="mt-20 w-full">
+        <DataTableWithColumnFilterDemo />
+      </div>
     </div>
   );
 };
