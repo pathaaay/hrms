d8e7b84a2d37cb44391c5782f17f0715509ea3c7
pathaaay <aayush.pathak@roimaint.com>
2026-02-09 16:55:50 +0530

    create get user booking action and query

--- frontend/src/components/common/data-table.tsx
+++ frontend/src/components/common/data-table.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useId, useMemo, useState } from "react";
 
 import { SearchIcon } from "lucide-react";
@@ -22,8 +20,6 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -42,186 +38,30 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { cn } from "@/lib/utils";
-
 declare module "@tanstack/react-table" {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     filterVariant?: "text" | "range" | "select";
   }
 }
 
-type Item = {
-  id: string;
-  product: string;
-  productImage: string;
-  fallback: string;
-  price: number;
-  availability: "In Stock" | "Out of Stock" | "Limited";
-  rating: number;
-};
-
-const columns: ColumnDef<Item>[] = [
-  {
-    header: "Product",
-    accessorKey: "product",
-    cell: ({ row }) => (
-      <div className="flex items-center gap-3">
-        <Avatar className="rounded-sm">
-          <AvatarImage
-            src={row.original.productImage}
-            alt={row.original.fallback}
-          />
-          <AvatarFallback className="text-xs">
-            {row.original.fallback}
-          </AvatarFallback>
-        </Avatar>
-        <div className="font-medium">{row.getValue("product")}</div>
-      </div>
-    ),
-  },
-  {
-    header: "Price",
-    accessorKey: "price",
-    cell: ({ row }) => <div>${row.getValue("price")}</div>,
-    enableSorting: false,
-    meta: {
-      filterVariant: "range",
-    },
-  },
-  {
-    header: "Availability",
-    accessorKey: "availability",
-    cell: ({ row }) => {
-      const availability = row.getValue("availability") as string;
-
-      const styles = {
-        "In Stock":
-          "bg-green-600/10 text-green-600 focus-visible:ring-green-600/20 dark:bg-green-400/10 dark:text-green-400 dark:focus-visible:ring-green-400/40 [a&]:hover:bg-green-600/5 dark:[a&]:hover:bg-green-400/5",
-        "Out of Stock":
-          "bg-destructive/10 [a&]:hover:bg-destructive/5 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 text-destructive",
-        Limited:
-          "bg-amber-600/10 text-amber-600 focus-visible:ring-amber-600/20 dark:bg-amber-400/10 dark:text-amber-400 dark:focus-visible:ring-amber-400/40 [a&]:hover:bg-amber-600/5 dark:[a&]:hover:bg-amber-400/5",
-      }[availability];
-
-      return (
-        <Badge
-          className={(cn("border-none focus-visible:outline-none"), styles)}
-        >
-          {row.getValue("availability")}
-        </Badge>
-      );
-    },
-    enableSorting: false,
-    meta: {
-      filterVariant: "select",
-    },
-  },
-  {
-    header: "Rating",
-    accessorKey: "rating",
-    cell: ({ row }) => <div>{row.getValue("rating")}</div>,
-    meta: {
-      filterVariant: "range",
-    },
-  },
-];
-
-const items: Item[] = [
-  {
-    id: "1",
-    product: "Black Chair",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-1.png",
-    fallback: "BC",
-    price: 159,
-    availability: "In Stock",
-    rating: 3.9,
-  },
-  {
-    id: "2",
-    product: "Nike Jordan",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-2.png",
-    fallback: "NJ",
-    price: 599,
-    availability: "Limited",
-    rating: 4.4,
-  },
-  {
-    id: "3",
-    product: "OnePlus 7 Pro",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-3.png",
-    fallback: "O7P",
-    price: 1299,
-    availability: "Out of Stock",
-    rating: 3.5,
-  },
-  {
-    id: "4",
-    product: "Nintendo Switch",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-4.png",
-    fallback: "NS",
-    price: 499,
-    availability: "In Stock",
-    rating: 4.9,
-  },
-  {
-    id: "5",
-    product: "Apple magic mouse",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-5.png",
-    fallback: "AMM",
-    price: 970,
-    availability: "Limited",
-    rating: 4.1,
-  },
-  {
-    id: "6",
-    product: "Apple watch",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-6.png",
-    fallback: "AW",
-    price: 1500,
-    availability: "Limited",
-    rating: 3.1,
-  },
-  {
-    id: "7",
-    product: "Casio G-Shock",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-8.png",
-    fallback: "CGS",
-    price: 194,
-    availability: "Out of Stock",
-    rating: 1.5,
-  },
-  {
-    id: "8",
-    product: "RayBan Sunglasses",
-    productImage:
-      "https://cdn.shadcnstudio.com/ss-assets/products/product-10.png",
-    fallback: "RBS",
-    price: 199,
-    availability: "Out of Stock",
-    rating: 2.4,
-  },
-];
-
-const DataTableWithColumnFilterDemo = () => {
+const DataTable = <T,>({
+  data,
+  columns,
+}: {
+  data: T[];
+  columns: ColumnDef<T>[];
+}) => {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const [sorting, setSorting] = useState<SortingState>([
     {
-      id: "price",
+      id: "name",
       desc: false,
     },
   ]);
 
   const table = useReactTable({
-    data: items,
+    data: data,
     columns,
     state: {
       sorting,
@@ -245,14 +85,8 @@ const DataTableWithColumnFilterDemo = () => {
           <div className="w-44">
             <Filter column={table.getColumn("product")!} />
           </div>
-          <div className="w-36">
-            <Filter column={table.getColumn("price")!} />
-          </div>
           <div className="w-44">
             <Filter column={table.getColumn("availability")!} />
-          </div>
-          <div className="w-36">
-            <Filter column={table.getColumn("rating")!} />
           </div>
         </div>
         <Table>
@@ -339,44 +173,6 @@ function Filter({ column }: { column: Column<any, unknown> }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [column.getFacetedUniqueValues(), filterVariant]);
 
-  if (filterVariant === "range") {
-    return (
-      <div className="*:not-first:mt-2">
-        <Label>{columnHeader}</Label>
-        <div className="flex">
-          <Input
-            id={`${id}-range-1`}
-            className="flex-1 rounded-r-none [-moz-appearance:_textfield] focus:z-10 [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-            value={(columnFilterValue as [number, number])?.[0] ?? ""}
-            onChange={(e) =>
-              column.setFilterValue((old: [number, number]) => [
-                e.target.value ? Number(e.target.value) : undefined,
-                old?.[1],
-              ])
-            }
-            placeholder="Min"
-            type="number"
-            aria-label={`${columnHeader} min`}
-          />
-          <Input
-            id={`${id}-range-2`}
-            className="-ms-px flex-1 rounded-l-none [-moz-appearance:_textfield] focus:z-10 [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-            value={(columnFilterValue as [number, number])?.[1] ?? ""}
-            onChange={(e) =>
-              column.setFilterValue((old: [number, number]) => [
-                old?.[0],
-                e.target.value ? Number(e.target.value) : undefined,
-              ])
-            }
-            placeholder="Max"
-            type="number"
-            aria-label={`${columnHeader} max`}
-          />
-        </div>
-      </div>
-    );
-  }
-
   if (filterVariant === "select") {
     return (
       <div className="*:not-first:mt-2">
@@ -423,4 +219,4 @@ function Filter({ column }: { column: Column<any, unknown> }) {
   );
 }
 
-export default DataTableWithColumnFilterDemo;
+export default DataTable;
--- frontend/src/pages/protected/games/bookings/index.tsx
+++ frontend/src/pages/protected/games/bookings/index.tsx
@@ -1,12 +1,68 @@
-import DataTableWithColumnFilterDemo from "@/components/common/data-table";
+import DataTable from "@/components/common/data-table";
 import { GoBackBtn } from "@/components/shared/go-back-btn";
+import { Badge } from "@/components/ui/badge";
+import { useFetchUserGameBookings } from "@/hooks/user/use-game-bookings";
+import type { IGameBooking } from "@/lib/types/game";
+import { cn } from "@/lib/utils";
+import type { ColumnDef } from "@tanstack/react-table";
+
+const columns: ColumnDef<IGameBooking>[] = [
+  {
+    header: "Name",
+    accessorKey: "name",
+    cell: ({ row }) => <div>${row.getValue("price")}</div>,
+    enableSorting: false,
+    meta: {
+      filterVariant: "select",
+    },
+  },
+  {
+    header: "Status",
+    accessorKey: "status",
+    cell: ({ row }) => {
+      const availability: string = row.getValue("status");
+
+      const styles = {
+        "In Stock":
+          "bg-green-600/10 text-green-600 focus-visible:ring-green-600/20 dark:bg-green-400/10 dark:text-green-400 dark:focus-visible:ring-green-400/40 [a&]:hover:bg-green-600/5 dark:[a&]:hover:bg-green-400/5",
+        "Out of Stock":
+          "bg-destructive/10 [a&]:hover:bg-destructive/5 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 text-destructive",
+        Limited:
+          "bg-amber-600/10 text-amber-600 focus-visible:ring-amber-600/20 dark:bg-amber-400/10 dark:text-amber-400 dark:focus-visible:ring-amber-400/40 [a&]:hover:bg-amber-600/5 dark:[a&]:hover:bg-amber-400/5",
+      }[availability];
+
+      return (
+        <Badge
+          className={(cn("border-none focus-visible:outline-none"), styles)}
+        >
+          {row.getValue("availability")}
+        </Badge>
+      );
+    },
+    enableSorting: false,
+    meta: {
+      filterVariant: "select",
+    },
+  },
+  {
+    header: "Rating",
+    accessorKey: "rating",
+    cell: ({ row }) => <div>{row.getValue("rating")}</div>,
+    meta: {
+      filterVariant: "range",
+    },
+  },
+];
 
 export const UserGameBookingPage = () => {
+  const { bookings, isPending } = useFetchUserGameBookings();
+  if (isPending) return;
+
   return (
     <div className="flex flex-col items-center gap-2 relative">
       <GoBackBtn to={"/games"} />
       <div className="mt-20 w-full">
-        <DataTableWithColumnFilterDemo />
+        <DataTable columns={columns} data={bookings || []} />
       </div>
     </div>
   );
