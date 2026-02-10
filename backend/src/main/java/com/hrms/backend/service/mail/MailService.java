58c348b2115262cbf53d825956071f9086c91cc9
pathaaay <aayush.pathak@roimaint.com>
2026-02-10 13:35:48 +0530

    add mail service cnfigure mail service changes in api response types

---
+++ backend/src/main/java/com/hrms/backend/service/mail/MailService.java
@@ -0,0 +1,29 @@
+package com.hrms.backend.service.mail;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+    @Value("${MAIL_USER}")
+    private String sender;
+
+    public void sendEmail(String[] to, String subject, String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setFrom(sender);
+        helper.setSubject(subject);
+        helper.setText(body, true);
+        mailSender.send(message);
+    }
+}
