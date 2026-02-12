e4d7bf4c21dee241f553dcba36ff0c2a51a92964
pathaaay <aayush.pathak@roimaint.com>
2026-02-12 18:41:44 +0530

    add dropdown for jobs in sidebar

--- frontend/src/components/shared/sidebar-links.tsx
+++ frontend/src/components/shared/sidebar-links.tsx
@@ -3,27 +3,70 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import { navLinks } from "@/lib/constants";
 import { NavLink } from "react-router";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
+import { ChevronRight } from "lucide-react";
 
 export function SidebarLinks() {
   return (
     <SidebarGroup>
       <SidebarMenu>
-        {navLinks.map((item) => (
-          <SidebarMenuItem key={item.label}>
-            <SidebarMenuButton tooltip={item.label} asChild>
-              <NavLink
-                className={"[.active]:bg-primary/20 [.active]:text-primary"}
-                to={item.url}
+        {navLinks.map((item) => {
+          if (item?.items && item?.items?.length > 0)
+            return (
+              <Collapsible
+                key={item.label}
+                asChild
+                className="group/collapsible"
               >
-                <item.icon />
-                <span>{item.label}</span>
-              </NavLink>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        ))}
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip={item.label}>
+                      {item.icon && <item.icon />}
+                      <span>{item.label}</span>
+                      <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {item.items?.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.label}>
+                          <SidebarMenuSubButton asChild>
+                            <NavLink className={"[.active]:bg-secondary [.active]:text-primary"} to={subItem.url}>
+                              <span>{subItem.label}</span>
+                            </NavLink>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            );
+
+          return (
+            <SidebarMenuItem key={item.label}>
+              <SidebarMenuButton tooltip={item.label} asChild>
+                <NavLink
+                  className={"[.active]:bg-secondary [.active]:text-primary"}
+                  to={item.url}
+                >
+                  <item.icon />
+                  <span>{item.label}</span>
+                </NavLink>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          );
+        })}
       </SidebarMenu>
     </SidebarGroup>
   );
