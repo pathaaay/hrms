67a6275aeecfe56d9ffd1278725e90f014d03238
pathaaay <aayush.pathak@roimaint.com>
2026-02-12 12:51:21 +0530

    add validation on request dto

--- backend/src/main/java/com/hrms/backend/dto/job/request/JobReferralEmailRequestDTO.java
+++ backend/src/main/java/com/hrms/backend/dto/job/request/JobReferralEmailRequestDTO.java
@@ -1,5 +1,6 @@
 package com.hrms.backend.dto.job.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +9,6 @@ import java.util.Set;
 @Getter
 @Setter
 public class JobReferralEmailRequestDTO {
+    @NotBlank(message = "At least 1 email is required")
     private Set<String> emails;
 }
