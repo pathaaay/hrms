235a370b70ab76b33a5730514df552b918e88b8c
pathaaay <aayush.pathak@roimaint.com>
2026-02-13 12:16:18 +0530

    bug fix on getting referrals
    - send jd file path if it's present (only for assigned referrals)

--- backend/src/main/java/com/hrms/backend/repository/job/JobReferralRepo.java
+++ backend/src/main/java/com/hrms/backend/repository/job/JobReferralRepo.java
@@ -1,10 +1,7 @@
 package com.hrms.backend.repository.job;
 
 import com.hrms.backend.entities.jobs.referral.JobReferral;
-import com.hrms.backend.entities.jobs.referral.JobReferralReviewStatus;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,6 +11,6 @@ public interface JobReferralRepo extends JpaRepository<JobReferral, Long> {
     @Query("SELECT jr from JobReferral jr WHERE jr.sharedBy.id=:id AND jr.isDeleted=false")
     List<JobReferral> findMyReferrals(@Param("id") Long id);
 
-    @Query(value = "SELECT jr from JobReferral jr JOIN jr.job.jobReviewers jrv WHERE jr.job.isActive=true AND jr.isDeleted=false AND jrv.id=:id")
+    @Query(value = "SELECT jr from JobReferral jr JOIN jr.job.jdDocument JOIN jr.job.jobReviewers jrv WHERE jr.job.isActive=true AND jr.isDeleted=false AND jrv.id=:id")
     List<JobReferral> findAssignedJobReferrals(@Param("id") Long id);
 }
