92d76c83922fb542cfb940af4d0911ee787ad271
pathaaay <aayush.pathak@roimaint.com>
2026-02-16 12:02:09 +0530

    send notifications when creating travel

--- backend/src/main/java/com/hrms/backend/service/travel/TravelService.java
+++ backend/src/main/java/com/hrms/backend/service/travel/TravelService.java
@@ -2,11 +2,17 @@ package com.hrms.backend.service.travel;
 
 import com.hrms.backend.dto.travel.request.TravelRequestDTO;
 import com.hrms.backend.dto.travel.response.TravelResponseDTO;
+import com.hrms.backend.entities.localization.City;
+import com.hrms.backend.entities.notification.Notification;
 import com.hrms.backend.entities.travel.Travel;
 import com.hrms.backend.entities.user.User;
 import com.hrms.backend.repository.travel.TravelRepo;
 import com.hrms.backend.service.localization.CityService;
+import com.hrms.backend.service.mail.MailService;
+import com.hrms.backend.service.notification.NotificationService;
 import com.hrms.backend.service.user.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.modelmapper.ModelMapper;
@@ -20,8 +26,10 @@ import java.util.Set;
 public class TravelService {
     private final TravelRepo travelRepo;
     private final ModelMapper modelMapper;
-    private final UserService userService;
     private final CityService cityService;
+    private final UserService userService;
+    private final MailService mailService;
+    private final NotificationService notificationService;
 
     private Travel convertToEntity(TravelRequestDTO dto, User user) throws BadRequestException {
         Travel travel = modelMapper.map(dto, Travel.class);
@@ -66,8 +74,33 @@ public class TravelService {
         return convertToDTOList(travelRepo.findAllByIsDeletedAndTravelMembers_Id(false, user.getId()));
     }
 
-    public void createTravel(TravelRequestDTO dto, User user) throws BadRequestException {
-        travelRepo.save(convertToEntity(dto, user));
+    @Transactional
+    public void createTravel(TravelRequestDTO dto, User user) throws BadRequestException, MessagingException {
+
+        Travel travel = travelRepo.save(convertToEntity(dto, user));
+
+        String[] toEmails = new String[travel.getTravelMembers().size()];
+
+        int i = 0;
+        for (User singleUser : travel.getTravelMembers()) {
+            toEmails[i++] = singleUser.getEmail();
+            notificationService.addNotification("New Travel:" + dto.getTitle(), "Description: " + dto.getDescription(), singleUser);
+        }
+
+        City city = cityService.findById(dto.getCityId());
+        String htmlBody = """
+                <div>
+                <div>New Travel: %s</div>
+                <div>Description: %s</div>
+                <div>Start Date: %s</div>
+                <div>End Date: %s</div>
+                <div>City: %s</div>
+                <div>State: %s</div>
+                <div>Country: %s</div>
+                </div>
+                """.formatted(dto.getTitle(), dto.getDescription(), dto.getStartDate(), dto.getEndDate(), city.getName(), city.getState().getName(), city.getState().getCountry().getName());
+
+        mailService.sendEmail(toEmails, "New Travel: " + dto.getTitle(), htmlBody);
     }
 
     public void updateTravel(TravelRequestDTO dto, User user, Long travelId) throws BadRequestException {
