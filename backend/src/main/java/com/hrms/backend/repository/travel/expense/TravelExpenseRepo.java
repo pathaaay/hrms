61fb53025a1e785f70b6e7daca77bc10c7264920
pathaaay <aayush.pathak@roimaint.com>
2026-02-17 15:21:21 +0530

    add validation on expense amount

--- backend/src/main/java/com/hrms/backend/repository/travel/expense/TravelExpenseRepo.java
+++ backend/src/main/java/com/hrms/backend/repository/travel/expense/TravelExpenseRepo.java
@@ -7,15 +7,20 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface TravelExpenseRepo extends JpaRepository<TravelExpense, Long> {
 
     List<TravelExpense> findAllByTravel_IdAndIsDeletedAndCreatedBy_Id(Long travelId, Boolean isDeleted, Long userId);
+
     List<TravelExpense> findAllByTravel_IdAndIsDeleted(Long travelId, Boolean isDeleted);
 
     Optional<TravelExpense> findByIdAndIsDeleted(Long id, Boolean isDeleted);
+
+    @Query(value = "SELECT SUM(te.amount) from travel_expenses te WHERE te.is_deleted=0 AND DATEDIFF(d, :expenseDate,te.expense_date) = 0", nativeQuery = true)
+    Long getExpenseTotalByDate(@Param("expenseDate") Date expenseDate);
 
     @Modifying
     @Transactional
